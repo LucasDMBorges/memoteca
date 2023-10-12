d7a67146865e2d146d04d41c38d8ff237fdffc67
LucasDMBorges <dev.lucasdean@gmail.com>
2023-10-12 12:25:48 -0300

    feat: :sparkles: Imlementando paginação

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,7 +1,6 @@
+import { PensamentoService } from "./../pensamento.service";
 import { Component, OnInit } from "@angular/core";
-import { TextoGrande } from "./listar-pensamento.const";
 import { Pensamento } from "../pensamento";
-import { PensamentoService } from "../pensamento.service";
 
 @Component({
   selector: "app-listar-pensamento",
@@ -10,12 +9,23 @@ import { PensamentoService } from "../pensamento.service";
 })
 export class ListarPensamentoComponent implements OnInit {
   listaPensamentos: Pensamento[] = [];
+  paginaAtual: number = 1;
+  haMaisPensamentos: boolean = true;
 
-  constructor(private pensamentoService: PensamentoService) {}
+  constructor(private service: PensamentoService) {}
 
   ngOnInit(): void {
-    this.pensamentoService.listar().subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
       this.listaPensamentos = listaPensamentos;
+    });
+  }
+
+  carregarMaisPensamentos() {
+    this.service.listar(++this.paginaAtual).subscribe((listaPensamentos) => {
+      this.listaPensamentos.push(...listaPensamentos);
+      if (!listaPensamentos.length) {
+        this.haMaisPensamentos = false;
+      }
     });
   }
 }
